cb7d0e2f980216a2ad8727c4fe0da55132287616
dantdj <dantdj@gmail.com>
2014-08-22 11:46:15 +0100

    Implemented Back and Up A Level methods

--- SharpManager/MainWindow.xaml.cs
+++ SharpManager/MainWindow.xaml.cs
@@ -26,6 +26,10 @@ namespace SharpManager
             InitializeComponent();
         }
 
+        string currentDirectory;
+        Stack<System.Collections.IEnumerable> back_stack = new Stack<System.Collections.IEnumerable>();
+        Stack<string> previousDirectory_stack = new Stack<string>();
+
         private void MainWindow1_Loaded(object sender, RoutedEventArgs e)
         {
             WriteToGridView(Helpers.FileManagement.getDrives());
@@ -47,9 +51,19 @@ namespace SharpManager
             {
                 List<Helpers.FileProperties> fileList = new List<Helpers.FileProperties>();
                 fileList = Helpers.FileManagement.getDirectoryContents(item);
+                
                 TB_CurrentDirectory.Text = item;
+                currentDirectory = item;
+
+                SetPreviousReferences();
                 WriteToGridView(fileList);
             }
+        }
+
+        private void SetPreviousReferences()
+        {
+            back_stack.Push(FileGrid.ItemsSource);
+            previousDirectory_stack.Push(currentDirectory);
         }
 
         private void IntoNewDirectoryFromDataGrid(string filename)
@@ -57,7 +71,7 @@ namespace SharpManager
             string item = filename;
             if (TB_CurrentDirectory.Text != "")
             {
-                item = TB_CurrentDirectory.Text + "\\" + filename;
+                item = currentDirectory + "\\" + filename;
             }
             IntoNewDirectory(item);
         }
@@ -74,6 +88,18 @@ namespace SharpManager
         {
             var selectedRow = FileGrid.SelectedItem as Helpers.FileProperties;
             IntoNewDirectoryFromDataGrid(selectedRow.Filename);
+        }
+
+        private void B_Back_Click(object sender, RoutedEventArgs e)
+        {
+            FileGrid.ItemsSource = back_stack.Pop();
+            TB_CurrentDirectory.Text = previousDirectory_stack.Pop();
+        }
+
+        private void B_Up_A_Level_Click(object sender, RoutedEventArgs e)
+        {
+            string newDirectory = Directory.GetParent(currentDirectory).FullName;
+            IntoNewDirectory(newDirectory);
         }
     }
 }
