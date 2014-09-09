5363adaee5dfdbea2076a3069ecdaf50bbfc6c79
Daniel Temple <dantdj@gmail.com>
2014-09-09 11:35:32 +0100

    Cleaned up code to reflect conventions

--- SharpManager/Helpers/FileManagement.cs
+++ SharpManager/Helpers/FileManagement.cs
@@ -2,98 +2,72 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SharpManager.Helpers
 {
     class FileManagement
     {
-        public static List<FileProperties> getDrives()
+        public static List<FileProperties> GetDrives()
         {
             // Return a list of available drives
-            List<FileProperties> drives = new List<FileProperties>();
 
-            foreach(string drive in Directory.GetLogicalDrives())
-            {
-                drives.Add(new FileProperties(drive, "", "", "", ""));
-            }
-            return drives;
+            return Directory.GetLogicalDrives().Select(drive => new FileProperties(drive, "", "", "", "")).ToList();
         }
 
-        private static string getByteConversion(float bytes)
+        private static string GetByteConversion(float bytes)
         {
-            float bytesInKilobyte = 1024;
-            float bytesInMegabyte = 1048576;
-            float bytesInGigabyte = 1073741824;
+            const float bytesInKilobyte = 1024;
+            const float bytesInMegabyte = 1048576;
+            const float bytesInGigabyte = 1073741824;
 
             if (bytes > bytesInGigabyte)
             {
-                return Math.Round((bytes / bytesInGigabyte), 2).ToString() + " GB";
+                return Math.Round((bytes / bytesInGigabyte), 2) + " GB";
             }
 
-            else if (bytes > bytesInMegabyte)
+            if (bytes > bytesInMegabyte)
             {
-                return Math.Round((bytes / bytesInMegabyte), 2).ToString() + " MB";
+                return Math.Round((bytes / bytesInMegabyte), 2) + " MB";
             }
 
-            else if (bytes > bytesInKilobyte)
+            if (bytes > bytesInKilobyte)
             {
-                return Math.Round((bytes / bytesInKilobyte), 2).ToString() + " KB";
+                return Math.Round((bytes / bytesInKilobyte), 2) + " KB";
             }
 
-            else
-            {
-                return bytes + " B";
-            }
+            return bytes + " B";
         }
 
-        public static void createDirectory(string targetPath)
+        public static void CreateDirectory(string targetPath)
         {
-            System.IO.Directory.CreateDirectory(targetPath);
+            Directory.CreateDirectory(targetPath);
         }
     
-        public static List<FileProperties> getDirectoryContents(string dirToSearch)
+        public static List<FileProperties> GetDirectoryContents(string dirToSearch)
         {
-            List<FileProperties> files = getFileList(dirToSearch);
-            List<FileProperties> subDirectories = getDirectoryList(dirToSearch);
+            var files = GetFileList(dirToSearch);
+            var subDirectories = GetDirectoryList(dirToSearch);
+            var contentsOfDirectory = new List<FileProperties>();
 
-            List<FileProperties> contentsOfDirectory = new List<FileProperties>();
             contentsOfDirectory.AddRange(subDirectories);
             contentsOfDirectory.AddRange(files);
 
             return contentsOfDirectory;
         }
     
-        private static List<FileProperties> getFileList(string dirToSearch)
+        private static IEnumerable<FileProperties> GetFileList(string dirToSearch)
         {
-            List<FileProperties> files = new List<FileProperties>();
-
-            foreach (string file in Directory.GetFiles(dirToSearch))
-            {
-                FileInfo fileinfo = new FileInfo(file);
-                files.Add(new FileProperties(Path.GetFileName(file), getFileType(fileinfo.FullName), getByteConversion(fileinfo.Length), fileinfo.CreationTime.ToString(), fileinfo.LastWriteTime.ToString()));
-            }
-
-            return files;
+            return (from file in Directory.GetFiles(dirToSearch) let fileinfo = new FileInfo(file) select new FileProperties(Path.GetFileName(file), GetFileType(fileinfo.FullName), GetByteConversion(fileinfo.Length), fileinfo.CreationTime.ToString(), fileinfo.LastWriteTime.ToString())).ToList();
         }
 
-        private static List<FileProperties> getDirectoryList(string dirToSearch)
+        private static IEnumerable<FileProperties> GetDirectoryList(string dirToSearch)
         {
-            List<FileProperties> subDirectories = new List<FileProperties>();
-
-            foreach(string subDirectory in Directory.GetDirectories(dirToSearch))
-            {
-                DirectoryInfo directoryinfo = new DirectoryInfo(subDirectory);
-                subDirectories.Add(new FileProperties(Path.GetFileName(subDirectory), "Folder", "", directoryinfo.CreationTime.ToString(), directoryinfo.LastWriteTime.ToString()));
-            }
-
-            return subDirectories;
+            return (from subDirectory in Directory.GetDirectories(dirToSearch) let directoryinfo = new DirectoryInfo(subDirectory) select new FileProperties(Path.GetFileName(subDirectory), "Folder", "", directoryinfo.CreationTime.ToString(), directoryinfo.LastWriteTime.ToString())).ToList();
         }
 
-        private static string getFileType(string filepath)
+        private static string GetFileType(string filepath)
         {
-            Dictionary<string, string> file_type_dict = new Dictionary<string, string>
+            var fileTypeDict = new Dictionary<string, string>
             {
                 {".txt", "Text Document"},
                 {".exe", "Application"},
@@ -122,14 +96,14 @@ namespace SharpManager.Helpers
             };
             string filetype = "";
             string extension = Path.GetExtension(filepath);
-            if (file_type_dict.ContainsKey(extension))
+            if (extension != null && fileTypeDict.ContainsKey(extension))
             {
-                filetype = file_type_dict[extension];
+                filetype = fileTypeDict[extension];
             }
             return filetype;
         }
 
-        public static void openFile(string filename)
+        public static void OpenFile(string filename)
         {
             try
             {
--- SharpManager/MainWindow.xaml.cs
+++ SharpManager/MainWindow.xaml.cs
@@ -1,41 +1,32 @@
-﻿using System;
+﻿using System.Collections;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using SharpManager.Helpers;
 
 namespace SharpManager
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    ///     Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow
     {
+        private readonly Stack<IEnumerable> _backStack = new Stack<IEnumerable>();
+        private readonly Stack<string> _previousDirectoryStack = new Stack<string>();
+        private string _currentDirectory;
+
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        string currentDirectory;
-        Stack<System.Collections.IEnumerable> back_stack = new Stack<System.Collections.IEnumerable>();
-        Stack<string> previousDirectory_stack = new Stack<string>();
-
         private void MainWindow1_Loaded(object sender, RoutedEventArgs e)
         {
-            WriteToGridView(Helpers.FileManagement.getDrives());
+            WriteToGridView(FileManagement.GetDrives());
         }
 
-        private void WriteToGridView(List<Helpers.FileProperties> fileList)
+        private void WriteToGridView(IEnumerable<FileProperties> fileList)
         {
             FileGrid.ItemsSource = fileList;
         }
@@ -44,16 +35,15 @@ namespace SharpManager
         {
             if (File.Exists(item))
             {
-                Helpers.FileManagement.openFile(item);
+                FileManagement.OpenFile(item);
             }
 
             else
             {
-                List<Helpers.FileProperties> fileList = new List<Helpers.FileProperties>();
-                fileList = Helpers.FileManagement.getDirectoryContents(item);
-                
+                List<FileProperties> fileList = FileManagement.GetDirectoryContents(item);
+
                 TB_CurrentDirectory.Text = item;
-                currentDirectory = item;
+                _currentDirectory = item;
 
                 SetPreviousReferences();
                 WriteToGridView(fileList);
@@ -62,8 +52,8 @@ namespace SharpManager
 
         private void SetPreviousReferences()
         {
-            back_stack.Push(FileGrid.ItemsSource);
-            previousDirectory_stack.Push(currentDirectory);
+            _backStack.Push(FileGrid.ItemsSource);
+            _previousDirectoryStack.Push(_currentDirectory);
         }
 
         private void IntoNewDirectoryFromDataGrid(string filename)
@@ -71,7 +61,7 @@ namespace SharpManager
             string item = filename;
             if (TB_CurrentDirectory.Text != "")
             {
-                item = currentDirectory + "\\" + filename;
+                item = _currentDirectory + "\\" + filename;
             }
             IntoNewDirectory(item);
         }
@@ -86,19 +76,19 @@ namespace SharpManager
 
         private void FileGrid_MouseDoubleClick(object sender, MouseButtonEventArgs e)
         {
-            var selectedRow = FileGrid.SelectedItem as Helpers.FileProperties;
-            IntoNewDirectoryFromDataGrid(selectedRow.Filename);
+            var selectedRow = FileGrid.SelectedItem as FileProperties;
+            if (selectedRow != null) IntoNewDirectoryFromDataGrid(selectedRow.Filename);
         }
 
         private void B_Back_Click(object sender, RoutedEventArgs e)
         {
-            FileGrid.ItemsSource = back_stack.Pop();
-            TB_CurrentDirectory.Text = previousDirectory_stack.Pop();
+            FileGrid.ItemsSource = _backStack.Pop();
+            TB_CurrentDirectory.Text = _previousDirectoryStack.Pop();
         }
 
         private void B_Up_A_Level_Click(object sender, RoutedEventArgs e)
         {
-            string newDirectory = Directory.GetParent(currentDirectory).FullName;
+            string newDirectory = Directory.GetParent(_currentDirectory).FullName;
             IntoNewDirectory(newDirectory);
         }
     }
