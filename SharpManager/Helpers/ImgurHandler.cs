11db74eae3f8fe45d64125042fc62bee66832cde
Daniel Temple <dantdj@gmail.com>
2014-12-17 21:40:52 +0000

    Code to upload image to Imgur created
    Added the code needed to upload and get a response from Imgur

--- SharpManager/Helpers/ImgurHandler.cs
+++ SharpManager/Helpers/ImgurHandler.cs
@@ -1,12 +1,31 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Collections.Specialized;
+using System.IO;
+using System.Net;
+using System.Windows;
+using System.Xml.Linq;
 
 namespace SharpManager.Helpers
 {
     class ImgurHandler
     {
+        private const string ClientId = "0751ca59efd1d0d";
+
+        public static void UploadImage(string image)
+        {
+            if (image == null) throw new ArgumentNullException("image");
+            using (var w = new WebClient())
+            {
+                w.Headers.Add("Authorization", "Client-ID " + ClientId);
+                var values = new NameValueCollection
+                {
+                    {"image", Convert.ToBase64String(File.ReadAllBytes(@image))}
+                };
+
+                byte[] response = w.UploadValues("https://api.imgur.com/3/upload.xml", values);
+
+                MessageBox.Show(XDocument.Load(new MemoryStream(response)).ToString());
+            }
+        }
     }
 }
