8614a992ef418a8954f4f282adf7289d067f8caf
Daniel Temple <dantdj@gmail.com>
2015-02-19 14:39:24 +0000

    Comments and documentation file added to project
    All methods in applicable files have been commented, and a built
    documentation file has been added in SharpManager/docs

--- SharpManager/Helpers/FileManagement.cs
+++ SharpManager/Helpers/FileManagement.cs
@@ -9,15 +9,25 @@ using SharpManager.Properties;
 
 namespace SharpManager.Helpers
 {
-    internal class FileManagement
+    /// <summary>
+    /// A class containing a variety of useful methods for file management.
+    /// </summary>
+    internal static class FileManagement
     { 
-        public static List<FileProperties> GetDrives()
+        /// <summary>
+        /// Returns a list of the available drives on the system.
+        /// </summary>
+        /// <returns>List of available drives.</returns>
+        public static IEnumerable<FileProperties> GetDrives()
         {
-            // Return a list of available drives
-
             return Directory.GetLogicalDrives().Select(drive => new FileProperties(drive, "", "", "", "")).ToList();
         }
 
+        /// <summary>
+        /// Converts a byte value to its relevant megabyte or gigabyte value, depending on the size of the file, rounded to two decimal places.
+        /// </summary>
+        /// <param name="bytes">Number of bytes to convert</param>
+        /// <returns>Returns a constructed string with the calculated gigabyte/megabyte/kilobyte/byte value.</returns>
         private static string GetByteConversion(float bytes)
         {
             const float bytesInKilobyte = 1024;
@@ -42,12 +52,21 @@ namespace SharpManager.Helpers
             return bytes + " B";
         }
 
+        /// <summary>
+        /// Simple method to consolidate the Directory.CreateDirectory method into the program.
+        /// </summary>
+        /// <param name="targetPath">Contains the path that the new directory will be created at.</param>
         public static void CreateDirectory(string targetPath)
         {
             Directory.CreateDirectory(targetPath);
         }
 
-        public static List<FileProperties> GetDirectoryContents(string dirToSearch)
+        /// <summary>
+        /// Searches a directory to get a list of all first-level subdirectories and files within the directory, ready to display in the grid view.
+        /// </summary>
+        /// <param name="dirToSearch">Contains the directory being scanned.</param>
+        /// <returns>Returns the all files and folders found inside the directory being scanned.</returns>
+        public static IEnumerable<FileProperties> GetDirectoryContents(string dirToSearch)
         {
             IEnumerable<FileProperties> files = GetFileList(dirToSearch);
             IEnumerable<FileProperties> subDirectories = GetDirectoryList(dirToSearch);
@@ -59,6 +78,11 @@ namespace SharpManager.Helpers
             return contentsOfDirectory;
         }
 
+        /// <summary>
+        /// Gets the list of files within a directory, as well as grabbing a variety of properties about the files for display in the grid view.
+        /// </summary>
+        /// <param name="dirToSearch">Contains the directory being scanned.</param>
+        /// <returns>Returns a list of all files found in the directory being scanned.</returns>
         private static IEnumerable<FileProperties> GetFileList(string dirToSearch)
         {
             return (from file in Directory.GetFiles(dirToSearch)
@@ -69,6 +93,11 @@ namespace SharpManager.Helpers
                         fileinfo.LastWriteTime.ToString())).ToList();
         }
 
+        /// <summary>
+        /// Gets the list of first-level subdirectories within a directory, as well as grabbing a variety of properties about the subdirectories for display in the grid view.
+        /// </summary>
+        /// <param name="dirToSearch">Contains the directory being scanned.</param>
+        /// <returns>Returns a list of all first-level subdirectories inside the directory being scanned.</returns>
         private static IEnumerable<FileProperties> GetDirectoryList(string dirToSearch)
         {
             return (from subDirectory in Directory.GetDirectories(dirToSearch)
@@ -78,6 +107,13 @@ namespace SharpManager.Helpers
                         directoryinfo.CreationTime.ToString(), directoryinfo.LastWriteTime.ToString())).ToList();
         }
 
+        /// <summary>
+        /// Compares the extension of a file to a dictionary of file type formats to provide a more descriptive idea of what a given type of file is.
+        /// For example, a .png file would be given the description "Image file".
+        /// If the file extension cannot be found in the dictionary, the extension itself is returned for use in the grid view.
+        /// </summary>
+        /// <param name="filepath">The path to the file to be checked.</param>
+        /// <returns>The file type description associated with the extension.</returns>
         private static string GetFileType(string filepath)
         {
             var fileTypeDict = new Dictionary<string, string>();
@@ -101,11 +137,20 @@ namespace SharpManager.Helpers
             return filetype;
         }
 
+        /// <summary>
+        /// Takes a file to be moved into another location.
+        /// </summary>
+        /// <param name="filename">The path to the file that will be moved.</param>
+        /// <param name="destination">The path that the file will be moved to.</param>
         public static void MoveFile(string filename, string destination)
         {
             Directory.Move(filename, destination);
         }
 
+        /// <summary>
+        /// Opens a file in the default program associated with it. If the file is an image file that is compatible with the Image Previewer, open it in the Image Previewer.
+        /// </summary>
+        /// <param name="filename">The path of the file to be opened.</param>
         public static void OpenFile(string filename)
         {
             var imageExtensions = new List<string> {".jpg", ".png", ".bmp", ".tiff", ".tif", ".gif", ".ico"};
@@ -128,6 +173,10 @@ namespace SharpManager.Helpers
             }
         }
 
+        /// <summary>
+        /// Simple method to consolidate the File.Delete method into the program.
+        /// </summary>
+        /// <param name="filename">The path of the file to be deleted.</param>
         public static void DeleteFile(string filename)
         {
             File.Delete(filename);
--- SharpManager/MainWindow.xaml.cs
+++ SharpManager/MainWindow.xaml.cs
@@ -12,23 +12,44 @@ namespace SharpManager
     public partial class MainWindow
     {
         private readonly Stack<string> _previousDirectoryStack = new Stack<string>();
-        public string CurrentDirectory;
+        /// <summary>
+        /// The directory that the program is currently displaying.
+        /// </summary>
+        private string _currentDirectory;
 
+        /// <summary>
+        /// Initializer for the MainWindow.
+        /// </summary>
         public MainWindow()
         {
             InitializeComponent();
         }
 
+        /// <summary>
+        /// Once program has loaded, populate the grid view with the list of available drives on the system.
+        /// </summary>
+        /// <param name="sender">Object.</param>
+        /// <param name="e">RoutedEventArgs</param>
         private void MainWindow1_Loaded(object sender, RoutedEventArgs e)
         {
             WriteToGridView(FileManagement.GetDrives());
         }
 
+        /// <summary>
+        /// Sets the ItemsSource for the file grid to be equal to the list of files and directories
+        /// </summary>
+        /// <param name="fileList">Contains the list to be put into the file grid.</param>
         private void WriteToGridView(IEnumerable<FileProperties> fileList)
         {
             FileGrid.ItemsSource = fileList;
         }
 
+        /// <summary>
+        /// If the item passed into the function is a file, that file is opened. If the item is a folder, the function will
+        /// push the current directory string to a stack, before opening the new directory and putting
+        /// the new file list in the file grid in its place.
+        /// </summary>
+        /// <param name="item">The path of the item that was clicked on in the file grid.</param>
         private void IntoNewDirectory(string item)
         {
             if (File.Exists(item))
@@ -38,39 +59,54 @@ namespace SharpManager
 
             else
             {
-                List<FileProperties> fileList = FileManagement.GetDirectoryContents(item);
+                IEnumerable<FileProperties> fileList = FileManagement.GetDirectoryContents(item);
 
                 SetPreviousReferences();
 
                 TB_CurrentDirectory.Text = item;
-                CurrentDirectory = item;
+                _currentDirectory = item;
                 
                 WriteToGridView(fileList);
             }
         }
 
+        /// <summary>
+        /// Pushes the current directory to a stack to be returned if the user needs to go back.
+        /// </summary>
         private void SetPreviousReferences()
         {
-            _previousDirectoryStack.Push(CurrentDirectory);
+            _previousDirectoryStack.Push(_currentDirectory);
         }
 
+        /// <summary>
+        /// Handles going into a new directory by selecting a cell on the file grid.
+        /// </summary>
+        /// <param name="filename">The file or folder selected.</param>
         private void IntoNewDirectoryFromDataGrid(string filename)
         {
             string item = filename;
             
             if (TB_CurrentDirectory.Text != "")
             {
-                item = CurrentDirectory + "\\" + filename;
+                item = _currentDirectory + "\\" + filename;
             }
             IntoNewDirectory(item);
         }
 
-         private void RefreshGridView()
+        /// <summary>
+        /// Reloads the file grid, reflecting changes in any items that have been updated.
+        /// </summary>
+        private void RefreshGridView()
         {
-            List<FileProperties> fileList = FileManagement.GetDirectoryContents(CurrentDirectory);
+            IEnumerable<FileProperties> fileList = FileManagement.GetDirectoryContents(_currentDirectory);
             WriteToGridView(fileList);
         }
 
+        /// <summary>
+        /// Opens the currently selected item if the enter key has been pressed.
+        /// </summary>
+        /// <param name="sender">Object.</param>
+        /// <param name="e">KeyEventArgs.</param>
         private void TB_CurrentDirectory_KeyDown(object sender, KeyEventArgs e)
         {
             if (e.Key == Key.Enter)
@@ -79,12 +115,22 @@ namespace SharpManager
             }
         }
 
+        /// <summary>
+        /// Opens an item if it is double-clicked.
+        /// </summary>
+        /// <param name="sender">Object.</param>
+        /// <param name="e">MouseButtonEventArgs.</param>
         private void FileGrid_MouseDoubleClick(object sender, MouseButtonEventArgs e)
         {
             var selectedRow = FileGrid.SelectedItem as FileProperties;
             if (selectedRow != null) IntoNewDirectoryFromDataGrid(selectedRow.Filename);
         }
 
+        /// <summary>
+        /// Grabs the previous directory from the stack and then opens the contents of that directory, if the "Back" button is clicked.
+        /// </summary>
+        /// <param name="sender">Object.</param>
+        /// <param name="e">RoutedEventArgs.</param>
         private void B_Back_Click(object sender, RoutedEventArgs e)
         {
             string item = _previousDirectoryStack.Pop();
@@ -92,23 +138,33 @@ namespace SharpManager
             {
                 WriteToGridView(FileManagement.GetDrives());
                 TB_CurrentDirectory.Text = "";
-                CurrentDirectory = "";
+                _currentDirectory = "";
                 return;
             }
-            List<FileProperties> fileList = FileManagement.GetDirectoryContents(item);
+            IEnumerable<FileProperties> fileList = FileManagement.GetDirectoryContents(item);
 
             TB_CurrentDirectory.Text = item;
-            CurrentDirectory = item;
+            _currentDirectory = item;
 
             WriteToGridView(fileList);
         }
 
+        /// <summary>
+        /// Opens the parent directory of the currently open directory, if the "Up A Level" button is clicked.
+        /// </summary>
+        /// <param name="sender">Object.</param>
+        /// <param name="e">RoutedEventArgs</param>
         private void B_Up_A_Level_Click(object sender, RoutedEventArgs e)
         {
-            string newDirectory = Directory.GetParent(CurrentDirectory).FullName;
+            string newDirectory = Directory.GetParent(_currentDirectory).FullName;
             IntoNewDirectory(newDirectory);
         }
 
+        /// <summary>
+        /// Opens the current directory again, if the "Refresh" button is clicked.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void B_Refresh_Click(object sender, RoutedEventArgs e)
         {
             RefreshGridView();
